e6bd4ace6765b9edaff83a983c7c39f0dcd367f9
Gautam Jakhar <108416604+realgautamjakhar@users.noreply.github.com>
2022-12-26 17:13:47 +0530

    netlify

--- src/Context/UserContext.jsx
+++ src/Context/UserContext.jsx
@@ -1,8 +1,5 @@
 import { useState } from "react";
 import { createContext } from "react";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 const UserContext = createContext();
 
 export const UserState = ({ children }) => {
